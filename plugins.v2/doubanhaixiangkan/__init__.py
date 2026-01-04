26ab7b8eda72d77e6bedf560fbb0d3b691015839
vvwwvv <budongkejivivi@gmail.com>
2026-01-04 14:24:52 +0800

    Update __init__.py

--- plugins.v2/doubanhaixiangkan/__init__.py
+++ plugins.v2/doubanhaixiangkan/__init__.py
@@ -35,9 +35,9 @@ class DoubanHaixiangkan(_PluginBase):
     # 插件图标
     plugin_icon = "douban.png"
     # 插件版本
-    plugin_version = "0.2"
+    plugin_version = "0.3"
     # 插件作者
-    plugin_author = "jxxghp,dwhmofly,Vivi,Gemini"
+    plugin_author = "jxxghp,dwhmofly,Vivi"
     # 作者主页
     author_url = "https://github.com/vivibudong"
     # 插件配置项ID前缀
